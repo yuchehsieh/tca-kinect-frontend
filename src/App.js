bc49ea4adaf1a383c46d4468fb16d58b57d942c5
Murphy Hsieh <murphyhsieh@leandev.io>
2020-11-12 21:09:58 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from "react";
+import axios from 'axios';
+import QRCode from "react-qr-code";
+
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [images, setImages] = useState([]);
+
+
+    const fetchImages = async () => {
+        const response = await axios({
+            method: 'get',
+            url: 'https://api.imgur.com/3/album/nM0DMMs/images',
+            headers: {
+                'Authorization': 'Client-ID d614e7ae6ec0a06'
+            }
+        });
+        let images = response.data.data;
+        console.log(images);
+        setImages(images);
+    };
+
+    useEffect(() => {
+        setInterval(() => {
+            fetchImages();
+        }, 5000)
+    }, []);
+
+    let qrcode = images.length >= 1
+        ? images[images.length-1].link
+        : "";
+
+    console.log(qrcode);
+
+    return (
+        <div className="App">
+
+            <div className="Container">
+
+
+                <div className="Left">
+
+                    <div
+                        style={{
+                            width: "40vw",
+                            height: "40vh",
+                            background: `url(${images[images.length-1]?.link}) center center / cover no-repeat`,
+                            backgroundSize: 'cover'
+                        }}
+                    />
+
+                </div>
+
+
+                <div className="Right">
+
+
+                    {
+                        images.length >= 1
+                        &&
+                        <QRCode value={qrcode}/>
+                    }
+
+
+                </div>
+
+
+            </div>
+        </div>
+    );
 }
 
 export default App;
