87d168b978cc1004db95c7b13a3ab1fdfbe1078c
Murphy Hsieh <murphyhsieh@leandev.io>
2020-11-27 12:49:19 +0800

    Change: styles

--- src/pages/Result.js
+++ src/pages/Result.js
@@ -2,6 +2,7 @@ import React, {useState, useEffect} from "react";
 import axios from 'axios';
 import QRCode from "react-qr-code";
 import {useHistory} from 'react-router-dom';
+import { FaArrowDown } from 'react-icons/fa';
 
 import './Result.css';
 import {routePath, site_url, url_prefix} from "../utils/constants";
@@ -47,27 +48,31 @@ function Result() {
     return (
         <div className="Container">
 
-            <div className="Big-Title">
-                挑選你的數位分身
+            <div className="Title-Wrapper">
+                <div className="Big-Title" />
             </div>
 
-            {
-                Array.from(Array(16)).map((item, index) => {
-                    let currentImage = images[images.length - 1 - index];
-                    return (
-                        <div className="Fix-Element" key={index}>
-                            <div
-                                onClick={() => onAlbumImageClick(currentImage.link)}
-                                style={{
-                                    width: "80%",
-                                    height: "90%",
-                                    background: `url(${currentImage?.link}) center center / contain no-repeat`
-                                }}
-                            />
-                        </div>
-                    )
-                })
-            }
+            <div className="Element-Wrapper">
+                {
+                    Array.from(Array(16)).map((item, index) => {
+                        let currentImage = images[images.length - 1 - index];
+                        return (
+                            <div className="Fix-Element" key={index}>
+                                <div
+                                    onClick={() => onAlbumImageClick(currentImage.link)}
+                                    style={{
+                                        width: "40vw",
+                                        height:" calc(40vw * 0.5625 )",
+                                        background: `url(${currentImage?.link}) center center / cover no-repeat`,
+                                        boxShadow: currentImage?.link? "10px 10px 5px rgba(255, 255, 255, 0.25)" : ""
+                                    }}
+                                />
+
+                            </div>
+                        )
+                    })
+                }
+            </div>
 
             <div className={`Backdrop ${isModalOpen ? "Show" : ""}`} onClick={() => setIsModalOpen(false)}>
 
@@ -77,16 +82,40 @@ function Result() {
 
                     <div className="Left">
 
-
                         <div
                             style={{
                                 width: "70%",
                                 height: "80%",
-                                background: `url(${selectedImage}) center center / contain no-repeat`,
+                                background: `url(${selectedImage}) top center / contain no-repeat`,
+                                position: 'relative'
                             }}
-                        />
-
-
+                        >
+                            <div
+                                className="arrow bounce"
+                                style={{
+                                    left: '-90%'
+                                }}
+                            >
+                                <FaArrowDown />
+                            </div>
+                            <p
+                                style={{
+                                    position: 'absolute',
+                                    left: '50%',
+                                    bottom: '0',
+                                    transform: 'translate(-50%, -50%)',
+                                    width: 'fit-content',
+                                    fontSize: '2em',
+                                    fontWeight: 'bold',
+                                    color: 'white'
+                                }}
+                            >
+                                掃描下方 QRCode 到手機上查看
+                            </p>
+                            <div className="arrow bounce">
+                                <FaArrowDown />
+                            </div>
+                        </div>
                     </div>
 
 
